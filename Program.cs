b1627e516adc3129082c3fe1dbf09a86d3617f3c
SissyManson <komandira123@gmail.com>
2024-06-15 18:49:36 +0300

    Created base class

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿using System;
+
+namespace Vehicle_Rental_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle car = new Vehicle("Opel", "Astra", 15000m);
+
+            Console.WriteLine($"Test output\nBought {car.brand} {car.model} for ${car.value}.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Vehicle_Rental_System
+{
+    internal class Vehicle
+    {
+        public string brand { get; set; }
+        public string model {  get; set; }
+        public decimal value { get; set; }
+
+        // constructor
+        public Vehicle(string brand, string model,decimal value)
+        {
+            this.brand = brand;
+            this.model = model;
+            this.value = value;
+        }
+    }
+}
