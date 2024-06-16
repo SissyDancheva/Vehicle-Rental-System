5fe8eacc467846d65eefec4342a6dcdf4ce9d561
SissyManson <komandira123@gmail.com>
2024-06-16 18:16:41 +0300

    Updated Main method
    Invoice works for all derived classes

--- Program.cs
+++ Program.cs
@@ -6,10 +6,30 @@ namespace Vehicle_Rental_System
     {
         static void Main(string[] args)
         {
-            Vehicle car = new Vehicle("Opel", "Astra", 15000m);
+            /* 
+             * > INPUTS:
+             *    - Customer Name, Model, Brand, Value, ReservationStartDate, ReservationEndDate, ReturnDate, ...
+             * > Speciffic inputs for the derived classes:
+             *   - Car: SafetyRating: 1-5
+             *   - Motorcycle: RiderAge
+             *   - Cargo Van: DriverExperience
+            */
 
-            Console.WriteLine($"Test output\nBought {car.brand} {car.model} for ${car.value}.");
+
+            Car car = new("John Doe", "Mitsubishi", "Mirage", 15000, new DateTime(2024, 6, 3), new DateTime(2024, 6, 13), new DateTime(2024, 6, 13), 3);
+            Motorcycle motorcycle = new("Mary Johnson", "Triumph", "Tiger Sport 660", 10000, new DateTime(2024, 6, 3), new DateTime(2024, 6, 13), new DateTime(2024, 6, 13), 20);
+            CargoVan cargoVan = new ("John Markson", "Citroen", "Jumper", 20000, new DateTime(2024, 6, 3), new DateTime(2024, 6, 18), new DateTime(2024, 6, 13), 8);
+            
+            Invoice CarInvoice = new (car);
+            Invoice MotorcycleInvoice = new (motorcycle);
+            Invoice CargoVanInvoice = new (cargoVan);
+
+            CarInvoice.DisplayInvoice();
+            MotorcycleInvoice.DisplayInvoice();
+            CargoVanInvoice.DisplayInvoice();
+
             Console.ReadLine();
         }
+
     }
 }
